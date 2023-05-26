906f64de8734526eff6132ce2b03a499a764fcf3
uafrontender <mikhail.karkachov@gmail.com>
2023-05-26 22:23:14 +0300

    Separate Vite configurations for prod and dev

---
+++ prod.vite.config.ts
@@ -0,0 +1,35 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    define: { global: (() => { 
+        let globalVariable = 'globalThis'; 
+        try { 
+            // Try to import @safe-global/safe-apps-provider 
+            require.resolve('@safe-global/safe-apps-provider'); 
+            // Try to import @safe-global/safe-apps-sdk 
+            require.resolve('@safe-global/safe-apps-sdk'); 
+            // If both modules are found, return the custom global variable 
+            globalVariable = 'global'; 
+        } catch (e) { 
+            // If either module is not found, fallback to globalThis 
+            globalVariable = 'globalThis'; 
+        } return globalVariable; })() 
+    },
+    resolve: {
+        alias: {
+            process: 'process/browser',
+            util: 'util',
+        },
+    },
+    server: {
+        origin: 'http://localhost:3000',
+        port: 3000,
+        host: '0.0.0.0',
+        fs: {
+            strict: true,
+        }
+    },
+    plugins: [react()],
+})
